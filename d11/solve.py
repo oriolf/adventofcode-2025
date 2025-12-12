00190b8d88af5222ccc92739d8928959c97ca507
Oriol Flors Mas <oriol.flors.mas@gmail.com>
2025-12-12 23:35:45 +0100

    Solved day 11

--- d11/solve.py
+++ d11/solve.py
@@ -9,9 +9,23 @@ ggg: out
 hhh: ccc fff iii
 iii: out"""
 
+example2 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out"""
+
 
 def parse_input(input):
-    devices = {}
+    devices = {"out": []}
     for line in input:
         device, connections = line.split(":")
         connections = [x.strip() for x in connections.strip().split(" ")]
@@ -26,14 +40,44 @@ def count_paths(devices, origin, destiny):
     return sum(count_paths(devices, conn, destiny) for conn in devices[origin])
 
 
+CACHE = {}
+
+
+def count_paths_dac_fft(devices, origin, destiny):
+    if origin == destiny:
+        return 1
+
+    global CACHE
+    key = (origin, destiny)
+    if key in CACHE:
+        return CACHE[key]
+
+    res = sum(count_paths_dac_fft(devices, conn, destiny) for conn in devices[origin])
+    CACHE[key] = res
+    return res
+
+
 def solve1(input):
     devices = parse_input(input)
     return count_paths(devices, "you", "out")
 
 
 def solve2(input):
-    return 0
+    devices = parse_input(input)
+
+    global CACHE
+    CACHE = {}
+
+    svr_fft = count_paths_dac_fft(devices, "svr", "fft")
+    fft_dac = count_paths_dac_fft(devices, "fft", "dac")
+    dac_out = count_paths_dac_fft(devices, "dac", "out")
+
+    svr_dac = count_paths_dac_fft(devices, "svr", "dac")
+    dac_fft = count_paths_dac_fft(devices, "dac", "fft")
+    fft_out = count_paths_dac_fft(devices, "fft", "out")
+
+    return svr_fft * fft_dac * dac_out + svr_dac * dac_fft * fft_out
 
 
 assert solve1(example.split("\n")) == 5
-assert solve2(example.split("\n")) == 0
+assert solve2(example2.split("\n")) == 2
